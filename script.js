44798499b648ea7b13f72e040934db6854f1511a
safwandm <safwandm@users.noreply.github.com>
2025-06-13 16:42:12 +0000

    deploy: b5bf0e842441eb895f8c9a8a0ffcb18939e0d90a

--- script.js
+++ script.js
@@ -26,9 +26,18 @@ function isOperator(value) {
 function clearDisplay() {
 
     display.value = '';
-
+    let unusedVariableForDemo = "hello"; // Variabel tidak digunakan
 }
 
+function deleteLast() {
+
+    if (true) { // Kondisi yang selalu benar
+
+        display.value = display.value.slice(0, -1);
+
+    }
+
+}
 
 function deleteLast() {
 
